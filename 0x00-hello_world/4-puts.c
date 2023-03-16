66936b1505e5835ec0e3cb2862155541ab07b9b1
emogibam <eogidan22@gmail.com>
2023-03-16 14:55:08 -0700

    Adding task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * It is a print function
+ *main - prints the phrase ""Programming is like building a multiligual puzzle
+ *Return: 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
