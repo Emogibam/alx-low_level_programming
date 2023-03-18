3326674e3a008507493ca5f9a5523af90454a39e
emogibam <eogidan22@gmail.com>
2023-03-17 19:26:33 -0700

    Adding no spaces

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,20 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- *  * main - assigns a random number to int n everytime
- *   * it executes, and prints it
- *    * Return: Always 0 (Success)
- *     */
-int main()
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+  */
+int main(void)
 {
     int n;
-    
     srand(time(NULL));
     n = rand() - RAND_MAX / 2;
-    
     printf("%d ", n);
-    
     if (n > 0)
     {
-        printf("is positive\n");
+    	printf("is positive\n");
     }
     else if (n == 0)
     {
@@ -27,8 +23,6 @@ int main()
     else
     {
         printf("is negative\n");
-    }
-    
-    return 0;
+    }   
+    return(0);
 }
-
